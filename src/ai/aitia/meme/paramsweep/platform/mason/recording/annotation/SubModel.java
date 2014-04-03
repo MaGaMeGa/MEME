eb8f12480b5e1b0d4d849968fde33016b93c62a7
rbocsi <rbocsi@aitia.ai>
2014-04-03 15:58:12 +0200

    @SubModel annotation

---
+++ src/ai/aitia/meme/paramsweep/platform/mason/recording/annotation/SubModel.java
@@ -0,0 +1,24 @@
+package ai.aitia.meme.paramsweep.platform.mason.recording.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * The {@link SubModel} annotation should decorate members of a Simulation class (a model class). 
+ * 
+ * @author Rajmund Bocsi
+ *
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD})
+@Inherited
+public @interface SubModel {
+	/**
+	 * A list of classes that can be the dynamic type of the member. This list is the domain of the {@link SubModel} parameter. If the list is empty then
+	 * the framework will try to find all descendant classes (or implementors if the field static type is an interface).    
+	 */
+	Class<?>[] value() default {};
+}
